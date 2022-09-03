cf1f6cc849d2012657a71cdfffafab863d9b7196
Nikushin-Oleg <nikushin.94@mail.ru>
2022-09-03 19:24:48 +0300

    Update

--- Graf_for_Vicia.R
+++ Graf_for_Vicia.R
@@ -5,6 +5,8 @@ library(ggtext)
 library(glue)
 library(patchwork)
 library(RColorBrewer)
+library(Richtext)
+library(gridtext)
 install.packages('RColorBrewer')
 Vicia_mass <- Vicia_mass %>% 
   mutate(treatment = factor(Variant, levels = c('Control','10 mkM', 'His 0.5 mM', 'His 1 mM', 'Gln 1 mM', 
@@ -86,7 +88,7 @@ Vicia_Cell_wall_mass.share.shoot<- Vicia_Cell_wall_mass.share.shoot+
 
 
 (Vicia_Cell_wall_mass.share.root |  Vicia_Cell_wall_mass.share.shoot)+
-  plot_annotation(title = "Рисунок 2", subtitle = 'Влияние ионов меди и лигандов на массовую долю клеточной стенки',
+  plot_annotation(title = "Рисунок 3", subtitle = 'Влияние ионов меди и лигандов на массовую долю клеточной стенки',
                   tag_levels = 'A')
 ggsave('Массовая доля клеточной стенки.png', width = 6, height = 5)
 
@@ -106,7 +108,7 @@ Vicia_Copper_ENDOGEN_CONT_shoot_b<- Vicia_Copper_ENDOGEN_CONT_shoot %>%
   ggplot(aes(x = treatment, y = Copper_content_per_DW_shoot, fill = treatment))+
   stat_summary(fun.data = mean_se, geom = 'errorbar', width = 0.3)+
   stat_summary(fun.data = mean_se, geom = 'bar', show.legend = FALSE)+
-  labs(x = NULL, y = 'мкМ Cu/г DW корня')+
+  labs(x = NULL, y = 'мкМ Cu/г DW побега')+
   #scale_x_discrete(labels = c('Control','100 mkM','100 mkM<br> His 0,5 mM','100 mkM<br> His 1 mM',
   #'100 mkM<br> Gln 1 mM', '100 mkM<br> Gln 5 mM'))+
   theme_classic()
@@ -125,7 +127,7 @@ Vicia_Copper_ENDOGEN_CONT_root_C
 
 Vicia_Copper_ENDOGEN_CONT_root_A / Vicia_Copper_ENDOGEN_CONT_root_B / 
   (Vicia_Copper_ENDOGEN_CONT_root_C)+
-  plot_annotation(title = 'Рисунок 3', subtitle = 'Эндогенная концентрация ионов меди в корне', 
+  plot_annotation(title = 'Рисунок 4', subtitle = 'Эндогенная концентрация ионов меди в корне', 
                   tag_levels = 'A')
 
 ggsave('Эндогенная концентрация ионов меди в корне.png', width = 8, height = 10)
@@ -134,7 +136,7 @@ Vicia_Copper_ENDOGEN_CONT_shoot_A
 Vicia_Copper_ENDOGEN_CONT_shoot_b
 Vicia_Copper_ENDOGEN_CONT_shoot_C
 Vicia_Copper_ENDOGEN_CONT_shoot_A/Vicia_Copper_ENDOGEN_CONT_shoot_b/Vicia_Copper_ENDOGEN_CONT_shoot_C+
-  plot_annotation(title = 'Рисунок 4', subtitle = 'Эндогенная концентрация ионов меди в побеге', 
+  plot_annotation(title = 'Рисунок 5', subtitle = 'Эндогенная концентрация ионов меди в побеге', 
                   tag_levels = 'A')
 
 ggsave('Эндогенная концентрация ионов меди в побеге.png', width = 8, height = 10)
@@ -164,10 +166,10 @@ Vicia_Copper_DESORBTION_root.C <- Vicia_Copper_DESORBTION_root.C+
   
 
 (Vicia_Copper_DESORBTION_root.A / Vicia_Copper_DESORBTION_root.B / Vicia_Copper_DESORBTION_root.C)+
-  plot_annotation(title = 'Рисунок 5', subtitle = 'Десорбция КС корней', tag_levels = 'A')
+  plot_annotation(title = 'Рисунок 6', subtitle = 'Десорбция КС корней', tag_levels = 'A')
 Vicia_Copper_DESORBTION_root.B
 Vicia_Copper_DESORBTION_root.C
-ggsave('Десорбция КС корней.png', width = 6, height = 8.5)
+ggsave('Десорбция КС корней.png', width = 6, height = 9)
 
 
 
@@ -195,7 +197,7 @@ Vicia_Copper_DESORBTION_shoot.A
 Vicia_Copper_DESORBTION_shoot.B
 Vicia_Copper_DESORBTION_shoot.C
 (Vicia_Copper_DESORBTION_shoot.A / Vicia_Copper_DESORBTION_shoot.B / Vicia_Copper_DESORBTION_shoot.C)+
-  plot_annotation(title = 'Рисунок 6', subtitle = 'Десорбция КС побегов', tag_levels = 'A')
+  plot_annotation(title = 'Рисунок 7', subtitle = 'Десорбция КС побегов', tag_levels = 'A')
 ggsave('Десорбция КС побегов.png', width = 6, height = 8.5)
 
 
@@ -256,96 +258,11 @@ Vicia_Copper_DESORBTION_shoot.TRCW.A<- Vicia_Copper_DESORBTION_shoot.TRCW.A+
   geom_text(data=tibble(x = c(6), y = 10), aes(x=x, y=y, label = 'b'), inherit.aes = FALSE, size = 4)
 
 (Vicia_Copper_DESORBTION_root.TRCW.A | Vicia_Copper_DESORBTION_shoot.TRCW.A)+
-  plot_annotation(title = 'Рисунок 7', subtitle = 'Сравнение десорбции КС контрольных и опытных растений',
+  plot_annotation(title = 'Рисунок 8', subtitle = 'Сравнение десорбции КС контрольных и опытных растений',
                   tag_levels = 'A')
 ggsave('Десорбция КС Контроль VS опыт.png', width = 10, height = 8.5)
 
 
-Vicia_Copper_by_solution <- Vicia_Copper_by_solution[,-4]
-  colnames(Vicia_Copper_by_solution) <- c('Variant', 'mkM_Cu_per_FW', 'mkM_Cu_per_DW', 'Method.1')
-  Vicia_Copper_by_solution <- Vicia_Copper_by_solution %>% 
-    arrange(Variant) %>% 
-    mutate(Method = c(rep('10 mkM IP', 18), 
-                      rep('Gln 1 mM IP', 6), 
-                      rep('Gln 5 mM IP', 12),
-                      rep('His 0.5 mM IP', 3), 
-                      rep('His 1 mM IP', 9)))
-
-Vicia_copper_SORBTION <- Vicia_copper_SORBTION 
-colnames(Vicia_copper_SORBTION) <- c('Variant', 'mkM_Cu_per_FW', 'mkM_Cu_per_DW')
-Vicia_copper_SORBTION_1 <- Vicia_copper_SORBTION_1[,-c(4,5)]
-Vicia_copper_SORBTION_1 <- Vicia_copper_SORBTION_1 %>% 
-  filter(Variant %in% c('10 mkM', 'His 0.5 mM', 'His 1 mM','Gln 1 mM', 'Gln 5 mM')) %>% 
-  mutate(Method = factor(Variant, levels = c('10 mkM', 'His 0.5 mM', 'His 1 mM','Gln 1 mM', 'Gln 5 mM'))) %>% 
-  arrange(Method) %>% 
-  mutate(method.2 = c(rep('10 mkM CW', 33), 
-                      rep('His 0.5 mM CW', 6), 
-                      rep('His 1 mM CW', 12), 
-                      rep('Gln 1 mM CW', 12), 
-                      rep('Gln 5 mM CW', 18))) %>% 
-  mutate(Method.1 = factor(method.2, levels = c('10 mkM CW', 'His 0.5 mM CW', 'His 1 mM CW', 'Gln 1 mM CW', 'Gln 5 mM CW')))
-
-
-Vicia_comparison_CW_VS_Intact_Plants <- bind_rows(Vicia_Copper_by_solution,Vicia_copper_SORBTION_1)
-Vicia_comparison_CW_VS_Intact_Plants<- Vicia_comparison_CW_VS_Intact_Plants %>% 
-  mutate(treatment = factor(Variant, levels = c('10 mkM', 'His 0.5 mM', 'His 1 mM', 'Gln 1 mM', 'Gln 5 mM'))) %>% 
-  mutate(treatment.2 = factor(Method.1, levels = c('10 mkM CW','10 mkM IP',
-                                                   'His 0.5 mM CW','His 0.5 mM IP',
-                                                   'His 1 mM CW', 'His 1 mM IP',
-                                                   'Gln 1 mM CW', 'Gln 1 mM IP',
-                                                   'Gln 5 mM CW', 'Gln 5 mM IP')))
-
-
-Vicia_comparison_CW_VS_Intact_Plants_ggplot <- Vicia_comparison_CW_VS_Intact_Plants %>% 
-  filter(treatment.2 %in% c('10 mkM CW','10 mkM IP',
-                            'His 0.5 mM CW','His 0.5 mM IP',
-                            'His 1 mM CW', 'His 1 mM IP',
-                            'Gln 1 mM CW', 'Gln 1 mM IP',
-                            'Gln 5 mM CW', 'Gln 5 mM IP')) %>% 
-  ggplot(aes(x = treatment.2, y = mkM_Cu_per_DW, fill = treatment.2))+
-  stat_summary(fun.data = mean_se, geom = 'errorbar', width = 0.3)+
-  stat_summary(fun.data = mean_se, geom = 'bar', show.legend = FALSE)+
-  scale_x_discrete(labels = c('КС', 'Растение','КС', 'Растение'
-                              ,'КС', 'Растение','КС', 'Растение'
-                              ,'КС', 'Растение'))+
-  labs(x = NULL, y = 'mkM Cu / г DW корня')+
-  theme_classic()+
-  scale_fill_manual(values = c('steelblue1', 'indianred1', 
-                               'steelblue1', 'indianred1',
-                               'steelblue1','indianred1',
-                               'steelblue1','indianred1',
-                               'steelblue1','indianred1'))+
-  geom_vline(xintercept = 2.5)+
-  geom_vline(xintercept = 4.5)+
-  geom_vline(xintercept = 6.5)+
-  geom_vline(xintercept = 8.5)+
-  geom_richtext(data=tibble(x = c(1.5), y = 25), aes(x=x, y=y, label = '10 mkM Cu'), inherit.aes = FALSE, size = 4,
-                fill = NA, label.colour = NA)+
-  geom_richtext(data=tibble(x = c(3.5), y = 25), aes(x=x, y=y, label = '10 mkM Cu<br> His 0.5 mM'), inherit.aes = FALSE, size = 4,
-                fill = NA, label.colour = NA)+
-  geom_richtext(data=tibble(x = c(5.5), y = 25), aes(x=x, y=y, label = '10 mkM Cu<br> His 1 mM'), inherit.aes = FALSE, size = 4,
-                fill = NA, label.colour = NA)+
-  geom_richtext(data=tibble(x = c(7.5), y = 25), aes(x=x, y=y, label = '10 mkM Cu<br> Gln 1 mM'), inherit.aes = FALSE, size = 4,
-                fill = NA, label.colour = NA)+
-  geom_richtext(data=tibble(x = c(9.5), y = 25), aes(x=x, y=y, label = '10 mkM Cu<br> Gln 5 mM'), inherit.aes = FALSE, size = 4,
-                fill = NA, label.colour = NA)+
-  theme(axis.text.x = element_markdown())
-
-Vicia_comparison_CW_VS_Intact_Plants_ggplot<- Vicia_comparison_CW_VS_Intact_Plants_ggplot+
-  geom_text(data=tibble(x = c(1), y = 16), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(2), y = 18), aes(x=x, y=y, label = 'b'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(3), y = 17), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(4), y = 18), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(5), y = 15), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(6), y = 15), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(7), y = 20), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(8), y = 12), aes(x=x, y=y, label = 'b'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(9), y = 19), aes(x=x, y=y, label = 'a'), inherit.aes = FALSE, size = 4)+
-  geom_text(data=tibble(x = c(10), y = 8), aes(x=x, y=y, label = 'b'), inherit.aes = FALSE, size = 4)
-
-Vicia_comparison_CW_VS_Intact_Plants_ggplot + 
-  plot_annotation(title = 'Рисунок 8', subtitle = 'Сравнение сорбции по раствором с поглощением транспирирующими растениями')
-ggsave('Сравнение сорбции с поглощением по растворам.png', width = 8, height = 6)
 
 Vicia_Copper_by_solution
 Vicia_Copper_ENDOGEN_CONT_root_1 <- Vicia_Copper_ENDOGEN_CONT_root %>% 
@@ -371,6 +288,7 @@ Vicia_comparison_Oz_VS_IP <- Vicia_comparison_Oz_VS_IP %>%
                                                'His 1 mM IP', 'His 1 mM Oz',
                                                'Gln 1 mM IP', 'Gln 1 mM Oz',
                                                'Gln 5 mM IP', 'Gln 5 mM Oz')))
+#########################################################################################
 Vicia_comparison_Oz_VS_IP.ggplot <- Vicia_comparison_Oz_VS_IP %>% 
   filter(order.2 %in% c('10 mkM IP', '10 mkM Oz',
                         'His 0.5 mM IP', 'His 0.5 mM Oz',
@@ -417,8 +335,8 @@ Vicia_comparison_Oz_VS_IP.ggplot <- Vicia_comparison_Oz_VS_IP %>%
   geom_text(data = tibble(x= 10, y = 2.7), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
   theme_classic()
 Vicia_comparison_Oz_VS_IP.ggplot+
-  plot_annotation(title = 'Рисунок 9', subtitle = 'Сравнения озоления с поглощением транспирирующими растениями')
-ggsave('Сравнение Озоления с поглощением по растворам.png', width = 8, height = 6)
+  plot_annotation(title = 'Рисунок 9', subtitle = 'Сравнения концентрации меди в корнях с убылью металла из раствора после обработки растений.')
+ggsave('Сравнение Озоления с поглощением по растворам.png', width = 8, height = 8)
 
 
 
@@ -462,13 +380,12 @@ mutate(Method = factor(method.2, levels = c('10 mkM CW','10 mkM Oz',
                                             '100 mkM Gln 5 mM CW', '100 mkM Gln 5 mM Oz')))
 
 
-Sravnenie_desorbc_s_Ozol_1.A <- Sravnenie_desorbc_s_Ozol_1 %>% 
-  filter(Method %in% c('10 mkM CW','10 mkM Oz',
-                       'His 0.5 CW','His 0.5 Oz', 
-                       'His 1 mM CW','His 1 mM Oz',
-                       'Gln 1 mM CW','Gln 1 mM Oz',
-                       'Gln 5 mM CW','Gln 5 mM Oz'
-                       )) %>% 
+Sravnenie_desorbc_s_Ozol_1.B <- Sravnenie_desorbc_s_Ozol_1 %>% 
+  filter(Method %in% c('100 mkM CW','100 mkM Oz',
+                       '100 mkM His 0.5 mM CW','100 mkM His 0.5 mM Oz', 
+                       '100 mkM His 1 mM CW','100 mkM His 1 mM Oz',
+                       '100 mkM Gln 1 mM CW','100 mkM Gln 1 mM Oz',
+                       '100 mkM Gln 5 mM CW','100 mkM Gln 5 mM Oz')) %>% 
   ggplot(aes(x = Method, y = Copper_per_DW, fill = Method))+
   stat_summary(fun.data = mean_se, geom = 'errorbar', width = 0.3)+
   stat_summary(fun.data = mean_se, geom = 'bar', show.legend = FALSE)+
@@ -487,36 +404,41 @@ Sravnenie_desorbc_s_Ozol_1.A <- Sravnenie_desorbc_s_Ozol_1 %>%
   geom_vline(xintercept = 4.5)+
   geom_vline(xintercept = 6.5)+
   geom_vline(xintercept = 8.5)+
-  geom_richtext(data=tibble(x = 1.5, y = 22), aes(x=x, y=y, label = '10 mkM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
-  geom_richtext(data=tibble(x = 3.5, y = 22), aes(x=x, y=y, label = '10 mkM<br> His 0.5 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
-  geom_richtext(data=tibble(x = 5.5, y = 22), aes(x=x, y=y, label = '10 mkM<br> His 1 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
-  geom_richtext(data=tibble(x = 7.5, y = 22), aes(x=x, y=y, label = '10 mkM<br> Gln 1 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
-  geom_richtext(data=tibble(x = 9.5, y = 22), aes(x=x, y=y, label = '10 mkM<br> Gln 1 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
-  geom_text(data = tibble(x= 1, y = 17.5), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 2, y = 8.9), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 3, y = 17.5), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 4, y = 5), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 5, y = 15.5), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 6, y = 4), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 7, y = 19), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 8, y = 5), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 9, y = 19), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
-  geom_text(data = tibble(x= 10, y = 2.7), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
+  geom_richtext(data=tibble(x = 1.5, y = 180), aes(x=x, y=y, label = '100 mkM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
+  geom_richtext(data=tibble(x = 3.5, y = 180), aes(x=x, y=y, label = '100 mkM<br> His 0.5 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
+  geom_richtext(data=tibble(x = 5.5, y = 180), aes(x=x, y=y, label = '100 mkM<br> His 1 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
+  geom_richtext(data=tibble(x = 7.5, y = 180), aes(x=x, y=y, label = '100 mkM<br> Gln 1 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
+  geom_richtext(data=tibble(x = 9.5, y = 180), aes(x=x, y=y, label = '100 mkM<br> Gln 5 mM'), inherit.aes = FALSE, fill = NA, label.colour = NA, size = 5)+
+  geom_text(data = tibble(x= 1, y = 125), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 2, y = 58), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 3, y = 125), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 4, y = 20), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 5, y = 105), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 6, y = 20), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 7, y = 110), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 8, y = 20), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 9, y = 110), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
+  geom_text(data = tibble(x= 10, y = 18), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
   theme_classic()
   
 ggsave('Сравнение Десорбции с озолением.png', width = 8, height = 6)
 
 (Sravnenie_desorbc_s_Ozol_1.A | Sravnenie_desorbc_s_Ozol_1.B)+
   plot_annotation(title = 'Рисунок 10', subtitle = 'Сравнение десорбции с озолением', tag_levels = 'A')
-ggsave('Сравнение Десорбции с озолением.png', width = 12, height = 8)
+ggsave('Сравнение Десорбции с озолением.png', width = 12, height = 10)
 
 
 
 Vicia_Copper_DESORBTION_SHOOT_VS_ROOT %>% 
-  ggplot(aes(x = treatment.2, y = mkM_Cu_per_DWCW, fill = part))+
+  filter(CW_from %in% c('Root 10 mkM', 'Shoot 10 mkM', 
+                        'Root His 0.5 mM', 'Shoot His 0.5 mM', 
+                        'Root His 1 mM', 'Shoot His 1 mM',
+                        'Root Gln 1 mM','Shoot Gln 1 mM',
+                        'Root Gln 5 mM','Shoot Gln 5 mM')) %>% 
+  ggplot(aes(x = CW_from, y = desorbtion_per_DW, fill = Variant))+
   stat_summary(fun.data = mean_se, geom = 'errorbar', width = 0.3)+
   stat_summary(fun.data = mean_se, geom = 'bar', show.legend = FALSE)+
-  labs(x = NULL, y = 'mkM Cu на г DW КС')+
+  labs(x = NULL, y = 'mkM Cu на г DW КС')
   scale_x_discrete(labels = c('КС корня', 'КС побега',
                               'КС корня', 'КС побега',
                               'КС корня', 'КС побега'))+
@@ -526,11 +448,11 @@ Vicia_Copper_DESORBTION_SHOOT_VS_ROOT %>%
                                'steelblue1','indianred1'))+
   geom_vline(xintercept = 2.5)+
   geom_vline(xintercept = 4.5)+
-  geom_richtext(data=tibble(x = 1.5, y=50), aes(x=x, y=y, label = 'Металл'), inherit.aes = FALSE,
+  geom_richtext(data=tibble(x = 1.5, y=50), aes(x=x, y=y, label = '10 mkM CuCl2'), inherit.aes = FALSE,
                 fill = NA, label.colour = NA)+
-  geom_richtext(data=tibble(x = 3.5, y=50), aes(x=x, y=y, label = 'Металл<br> His 1 mM'), inherit.aes = FALSE,
+  geom_richtext(data=tibble(x = 3.5, y=50), aes(x=x, y=y, label = '10 mkM CuCl2<br> His 1 mM'), inherit.aes = FALSE,
                 fill = NA, label.colour = NA)+
-  geom_richtext(data=tibble(x = 5.5, y=50), aes(x=x, y=y, label = 'Металл<br> Gln 5 mm'), inherit.aes = FALSE,
+  geom_richtext(data=tibble(x = 5.5, y=50), aes(x=x, y=y, label = '10 mkM CuCl2<br> Gln 5 mm'), inherit.aes = FALSE,
                 fill = NA, label.colour = NA)+
   geom_text(data = tibble(x= 1, y = 38), aes(x = x, y=y, label='a'),inherit.aes = FALSE, size = 4)+
   geom_text(data = tibble(x= 2, y = 32), aes(x = x, y=y, label='b'),inherit.aes = FALSE, size = 4)+
--- Statistic.R
+++ Statistic.R
@@ -65,57 +65,7 @@ Vicia_comparison_Oz_VS_IP %>%
   group_by(Method.1) %>% 
   summarise(mean(mkM_Cu_per_DW)) %>% 
   View()
-Vicia_Copper_DESORBTION_shoot.x3 <- Vicia_Copper_DESORBTION_shoot %>% 
-  filter(Variant %in% c('10 mkM 450 ml', 'His 1 mM 450 ml', 'Gln 5 mM 450 ml', '30 mkM', 'His 3 mM 30 mkM', 'Gln 15 mM 30 mkM')) 
-Vicia_Copper_DESORBTION_shoot.x3 <- Vicia_Copper_DESORBTION_shoot.x3 %>% 
-  slice(-12, -13,-14)
-Vicia_Copper_DESORBTION_shoot.x3
- colnames(Vicia_Copper_DESORBTION_shoot.x3) <- c('Variant', 'mkM Cu per FW','mkM Cu per DW','mkM Cu per DWCW')
-Vicia_Copper_DESORBTION_SHOOT_VS_ROOT <- bind_rows(Vicia_Copper_DESORBTION_root.x3,Vicia_Copper_DESORBTION_shoot.x3)
-Vicia_Copper_DESORBTION_SHOOT_VS_ROOT <- Vicia_Copper_DESORBTION_SHOOT_VS_ROOT %>% 
-  mutate(part = c(rep('Root',39), rep('Shoot',14))) %>% 
-  mutate(treatment = c(rep('10 mkM Root', 3),
-                       rep('Gln 5 mM Root',3),
-                       rep('10 mkM Root', 12),
-                       rep('Gln 5 mM Root', 3),
-                       rep('10 mkM Root', 3),
-                       rep('His 1 mM Root', 3),
-                       rep('10 mkM Root', 3),
-                       rep('Gln 5 mM Root', 3),
-                       rep('His 1 mM Root', 3),
-                       rep('Gln 5 mM Root', 3),
-                       rep('10 mkM Shoot', 2),
-                       rep('His 1 mM Shoot', 3),
-                       rep('Gln 5 mM Shoot', 3),
-                       rep('10 mkM Shoot', 3),
-                       rep('Gln 5 mM Shoot', 3))) %>% 
-  mutate(treatment.2 = factor(treatment, levels = c('10 mkM Root','10 mkM Shoot',
-                                                    'His 1 mM Root','His 1 mM Shoot',
-                                                    'Gln 5 mM Root','Gln 5 mM Shoot'))) %>% 
-  mutate(treatment.3 = c(rep('10 mkM', 3),
-                       rep('Gln 5 mM',3),
-                       rep('10 mkM', 12),
-                       rep('Gln 5 mM', 3),
-                       rep('10 mkM', 3),
-                       rep('His 1 mM', 3),
-                       rep('10 mkM', 3),
-                       rep('Gln 5 mM', 3),
-                       rep('His 1 mM', 3),
-                       rep('Gln 5 mM', 3),
-                       rep('10 mkM', 2),
-                       rep('His 1 mM', 3),
-                       rep('Gln 5 mM', 3),
-                       rep('10 mkM', 3),
-                       rep('Gln 5 mM', 3)))
-mutate
-colnames(Vicia_Copper_DESORBTION_SHOOT_VS_ROOT) <- c('Variant',
-                                                     'mkM_Cu_per_FW',
-                                                     'mkM_Cu_per_DW',
-                                                     'mkM_Cu_per_DWCW',
-                                                     'part',
-                                                     'treatment',
-                                                     'treatment.2',
-                                                     'treatment.3')
+
 
 Vicia_Copper_DESORBTION_SHOOT_VS_ROOT.t.test <-  Vicia_Copper_DESORBTION_SHOOT_VS_ROOT %>% 
   filter(treatment.3 %in% c('His 1 mM'))
@@ -134,4 +84,33 @@ Vicia_pH.shoot.t.test <- Vicia_pH.shoot %>%
   filter(Variant %in% c('Control', 'Gln 1 mM'))
 t.test(pH_after_sorbtion.shoot ~ Variant, Vicia_pH.shoot.t.test, paired = FALSE)
 
+Vicia_Copper_DESORBTION_root
 
+Vicia_Copper_DESORBTION_root.1 <- Vicia_Copper_DESORBTION_root %>% 
+  arrange(Variant) %>% 
+  slice(-c(87:98)) %>% 
+  select(1:4) %>% 
+  mutate(CW_from = c(rep('Root 10 mkM',21),
+                     rep('Root 100 mkM',11),
+                     rep('Root 100 mkM Gln 1 mM', 3),
+                     rep('Root 100 mkM Gln 5 mM', 3),
+                     rep('Root 100 mkM His 0.5 mM', 3),
+                     rep('Root 100 mkM His 1 mM', 3),
+                     rep('Root 50 mkM', 6),
+                     rep('Root Gln 1 mM', 12),
+                     rep('Root Gln 5 mM', 12),
+                     rep('Root His 0.5 mM', 6),
+                     rep('Root His 1 mM', 6),
+                     rep('Root Tr 10 mkM', 9),
+                     rep('Root Tr Gln 5 mM', 9),
+                     rep('Root Tr His 1 mM', 9)))
+
+colnames(Vicia_Copper_DESORBTION_shoot.1) <- c('Variant', 'desorbtion_per_FW',
+                                              'desorbtion_per_DW', 'desorbtion_per_DWCW', 'CW_from')
+
+Vicia_Copper_DESORBTION_SHOOT_VS_ROOT <- bind_rows(Vicia_Copper_DESORBTION_shoot.1, Vicia_Copper_DESORBTION_root.1)
+Vicia_Copper_DESORBTION_SHOOT_VS_ROOT<- Vicia_Copper_DESORBTION_SHOOT_VS_ROOT %>% 
+  mutate(treatment_order = factor(Variant, levels = c('10 mkM', 'His 0.5 mM', 'His 1 mM', 'Gln 1 mM', 'Gln 5 mM',
+                                                       '100 mkM', '100 mkM His 0.5 mM', '100 mkM His 1 mM', '100 mkM Gln 1 mM', '100 mkM Gln 5 mM',
+                                                       '50 mkM', 'Tr 10 mkM', 'Tr His 1 mM', 'Tr Gln 5 mM')))
+  
